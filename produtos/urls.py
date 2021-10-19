142d858864fae719469348398f24ab50e199fa69
Alexsander V Junior <alexsandervieirajunior@hotmail.com>
2021-10-19 20:56:59 -0300

    Add tela de produtos

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, home_produtos
 
 namespace = 'produtos'
 
 urlpatterns = [
     path('', home, name="home"),
+    path('produtos/', home_produtos, name="home_produtos"),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -4,5 +4,8 @@ from .service import ProdutoService
 _SERVICE = ProdutoService()
 
 def home(request):
+    return render(request, 'home.html')
+
+def home_produtos(request):
     produtos = _SERVICE.buscar_todos_produtos()
-    return render(request, 'home.html', context={"produtos":produtos})
+    return render(request, 'home-produtos.html', context={"produtos":produtos})
