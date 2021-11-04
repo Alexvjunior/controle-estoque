2b5d36aca0428d28e7640617aec94e5f9dd1c10e
Alexsander V Junior <alexsandervieirajunior@hotmail.com>
2021-11-04 20:36:04 -0300

    Adicionado app historico

--- conf/settings.py
+++ conf/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     # APPS
     'produtos',
+    'historicos',
 
 ]
 
---
+++ historicos/serializers.py
@@ -0,0 +1,15 @@
+from .models import HistoricoModel
+from rest_framework import serializers
+from produtos.service import ProdutoService
+
+_SERVICE_PRODUTO = ProdutoService()
+
+class HistoricoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HistoricoModel
+        fields = ('id', 'produto_id', 'funcionario', 'tipo', 'descricao')
+
+    def create(self, validated_data):
+        produto = _SERVICE_PRODUTO.buscar_produto_por_codigo(validated_data.get('codigo_produto'))
+        validated_data['produto_id'] = produto.id
+        return super().create(validated_data)
